d276727a6eaec0eda530a20de0ddc17b94b3bd03
Charly Marchiaro <carlosmarchiaro@gmail.com>
2022-05-19 12:04:25 -0300

    added Python_Science_Course files

---
+++ curso/Python_Science_Course/Python_Science_Solution/02_Python_basics/Moduletest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def functionmodul(x):
+    y=x+3
+    print("yes the import worked")
+    return y
+
---
+++ curso/Python_Science_Course/Python_Science_Solution/02_Python_basics/myApp/greet.py
@@ -0,0 +1,6 @@
+
+
+def SayHello(name):
+    print("Hello " + name)
+    return
+
